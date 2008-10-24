881ceaa531b06ca6aff04aa4332c5e124751cb6d
Jonathan Lonowski <coiscir@users.noreply.github.com>
2008-10-24 07:00:29 +0000

    Tag 8.1024

---
+++ 8.1024/Rakefile.rb
@@ -0,0 +1,140 @@
+require 'rake'
+require 'builder'
+
+################################################################################
+# Prepare
+####
+ROOT = File.expand_path(File.dirname(__FILE__))
+LIBS = File.join(ROOT, 'lib')
+SRCS = File.join(ROOT, 'src')
+
+INCS = {
+  'type'   => File.join(SRCS, 'type', 'anchor.js'),
+  'crypto' => File.join(SRCS, 'crypto', 'anchor.js'),
+  'cookie' => File.join(SRCS, 'cookie', 'anchor.js'),
+  'query'  => File.join(SRCS, 'query', 'anchor.js')
+};
+DIRS = INCS.keys.sort
+
+################################################################################
+# Versioning
+####
+TIME = Time.now.utc
+VERS = File.join(ROOT, 'VERSION')
+PREC = ['prec1', 'prec2', 'prec3', 'prec4', 'prec5']
+$prec = nil # default = 2
+$vers = nil
+
+def setprecision(prec)
+  $prec = prec
+end
+
+def setserial(vers)
+  $vers = vers
+end
+
+def serial
+  if ($vers.nil?)
+    p = ($prec.is_a?(Numeric) && $prec > 0) ? $prec : 2
+    f = "%.#{p*2}f"
+    s = "0.%m#{'%d'if(p>1)}#{'%H'if(p>2)}#{'%M'if(p>3)}#{'%S'if(p>4)}"
+    setserial(sprintf(f, (TIME.strftime("%Y").to_f - 2000 + TIME.strftime(s).to_f)))
+  end
+  $vers
+end
+
+################################################################################
+# Setup
+####
+INCS.each_pair do |name,anch|
+  Builder.register(name, anch)
+end
+
+################################################################################
+# Tasks
+####
+task :default do
+  print [
+    "Tasks:",
+    "  build (all)",
+    "  clean (rm)",
+    "  status (st)",
+    "",
+    "Version Precision Tasks: (can only use 1)",
+    "  prec1 - Y.MM",
+    "  prec2 - Y.MMDD - default",
+    "  prec3 - Y.MMDDhh",
+    "  prec4 - Y.MMDDhhmm",
+    "  prec5 - Y.MMDDhhmmss",
+  ].join($/) + $/
+end
+
+task :prec1 do
+  setprecision(1)
+  PREC.reject{|p| p=='prec1'}.each{|p| Rake::Task[p].disable}
+end
+task :prec2 do
+  setprecision(2)
+  PREC.reject{|p| p=='prec2'}.each{|p| Rake::Task[p].disable}
+end
+task :prec3 do
+  setprecision(3)
+  PREC.reject{|p| p=='prec3'}.each{|p| Rake::Task[p].disable}
+end
+task :prec4 do
+  setprecision(4)
+  PREC.reject{|p| p=='prec4'}.each{|p| Rake::Task[p].disable}
+end
+task :prec5 do
+  setprecision(5)
+  PREC.reject{|p| p=='prec5'}.each{|p| Rake::Task[p].disable}
+end
+
+task :all => :build
+task :rm  => :clean
+task :st  => :status
+
+desc "Build all library scripts"
+task :build do
+  print $/ + '== Build' + ' :: ' + TIME.strftime('%Y-%m-%d %I:%M:%S') + ' :: ' + serial.to_s + $/
+  DIRS.each do |pkg|
+    libf = File.join(LIBS, "#{pkg}.js")
+    File.open(libf, "w+") do |lib|
+      lib << Builder.start(pkg, nil)
+      print ' + ' + File.split(libf)[1] + $/ if File.exists?(libf)
+    end
+  end
+end
+
+desc "Delete all library scripts"
+task :clean do
+  print $/ + '== Clean' + $/
+  DIRS.each do |pkg|
+    libf = File.join(LIBS, "#{pkg}.js")
+    print ' - ' + File.split(libf)[1] + $/ if File.exists?(libf) && File.delete(libf) > 0
+  end
+end
+
+desc "Check build status of library scrips"
+task :status do
+  print $/ + '== Status' + $/
+  DIRS.each do |pkg|
+    libf = File.join(LIBS, "#{pkg}.js")
+    print ' ' + (File.exists?(libf) ? '+' : '-') + ' ' + File.split(libf)[1] + $/
+  end
+end
+
+################################################################################
+# Monkey Patch
+####
+module Rake
+  class Task
+    def disable
+      @already_invoked = true
+    end
+    
+    def enable
+      @already_invoked = false
+    end
+  end
+end
---
+++ 8.1024/builder.rb
@@ -0,0 +1,92 @@
+require 'rubygems'
+require 'jsmin'
+require 'erb'
+
+module Builder
+  
+  PKGS = {}
+  
+  class Reader
+    def initialize(name, file, pkgs)
+      return if !Builder.registered?(name)
+      @save_reqs = true
+      @pkgs = pkgs.uniq.sort
+      
+      if (PKGS[name]['src'].nil?)
+        start = File.split(file)
+        Dir.chdir(start[0]) do
+          @src = PKGS[name]['src'] = reader(start[1])
+        end
+      else
+        @src = PKGS[name]['src']
+      end
+      self
+    end
+    
+    def require(*pkgs)
+      return ("<%= require " + pkgs.map{|p| p.inspect}.join(', ') + " %>") if @save_reqs
+      
+      pad = pkgs.first.is_a?(Numeric) ? pkgs.shift : 0
+      step = [].concat(@pkgs).concat(pkgs).uniq.sort
+      pkgs.reject{
+        |p| !Builder.registered?(p) || @pkgs.include?(p)
+      }.map{
+        |p| JSMin.minify(Builder.start(p, step)).gsub(/\n/, '').sub(/^ /, '')
+      }.map{
+        |p| p.gsub(/^/, (' ' * pad))
+      }.join($/)
+    end
+    
+    def include(*files)
+      pad = files.first.is_a?(Numeric) ? files.shift : 0
+      min = files.first == true ? files.shift : false
+      files.map{
+        |f| reader(f).strip
+      }.map{
+        |f| min ? JSMin.minify(f).gsub(/\n/, '').sub(/^ /, '') : f
+      }.map{
+        |f| f.gsub(/^/, (' ' * pad))
+      }.join($/)
+    end
+    
+    def to_s
+      final
+    end
+    
+    private
+    
+    def reader(file)
+      parse(IO.readlines(file).to_s)
+    end
+    
+    def parse(source)
+      ERB.new(source).result(binding)
+    end
+    
+    def final
+      @save_reqs = false
+      parse(@src).gsub(/[ \t]+$/m, '')
+    end
+  end
+
+  class << self
+    def registered?(name)
+      PKGS.keys.include?(name)
+    end
+    
+    def register(name, anchor)
+      return if !PKGS[name].nil?
+      return if !File.file?(anchor)
+      
+      PKGS[name] = {'start' => File.expand_path(anchor)}
+    end
+    
+    def start(name, pkgs)
+      return if !self.registered?(name)
+      
+      pkgs = [] if pkgs.nil?
+      Reader.new(name, PKGS[name]['start'], [name].concat(pkgs).uniq).to_s
+    end
+  end
+  
+end
