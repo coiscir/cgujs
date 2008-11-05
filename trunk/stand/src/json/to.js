73f95d0a6a4831fb7d604cd3702e98f77fa00a40
jonllmsed <jonllmsed@217474b8-4953-0410-b849-a73ab7b60919>
2008-11-05 05:26:32 +0000

    JSON: Added options.ascii for string encoding in JSON.to. Added options.relax.objkey and key write function for objects in JSON.to. Added tests for each. Cleaned up tests, adding topic lines.

--- trunk/stand/src/json/to.js
+++ trunk/stand/src/json/to.js
@@ -4,6 +4,7 @@
   this.to = this.stringify = function (input, options) {
     options = (function (o) { return {
       allkey : Type.limit(o.allkey, Boolean)         || false,
+      ascii  : Type.limit(o.ascii,  Boolean)         || false,
       relax  : Type.limit(o.relax,  Boolean, Object) || false,
       verify : Type.limit(o.verify, Boolean)         || false
     };})(options || {});
@@ -12,20 +13,40 @@
       var global = Type.limit(r, Boolean) || false;
       return {
         date    : Type.limit(r.date,    Boolean) || global,
-        keyword : Type.limit(r.keyword, Boolean) || global
+        keyword : Type.limit(r.keyword, Boolean) || global,
+        objkey  : Type.limit(r.objkey,  Boolean) || global
       };
     })(options.relax || false);
     
-    var object = function (input) {
-      var pairs = [], k, v;
-      for (var prop in input) {
-        if (options.allkey || input.propertyIsEnumerable(prop)) {
-          k = string(prop);
-          v = value(input[prop]);
-          if (Type.is_a(v, String)) pairs.push(k + ' : ' + v);
+    var string = function (input) {
+      var result = '', enc;
+      var against = options.ascii ? /[\\"]|[^\x20-\x7e]/ : /[\x00-\x1f\\"\x7f]/;
+      var specials = {
+        '"'  : '\\"',
+        '\b' : '\\b',
+        '\f' : '\\f',
+        '\n' : '\\n',
+        '\r' : '\\r',
+        '\t' : '\\t',
+        '\\' : '\\\\'
+      };
+      while (input.length > 0) {
+        if (match = input.match(against)) {
+          enc = padnum(4, match[0].charCodeAt(0).toString(16));
+          
+          result += input.slice(0, match.index);
+          result += specials[match[0]] ? specials[match[0]] : ('\\u' + enc);
+          input  = input.slice(match.index + match[0].length);
+        } else {
+          result += input; input = '';
         }
       }
-      return '{' + pairs.join(', ') + '}';
+      return '"' + result + '"';
+    };
+    
+    var date = function (input) {
+      if (!options.relax.date) return undefined;
+      return string(Time.phputc("Y-m-d\\TH:i:s.u\\Z", input));
     };
     
     var array = function (input) {
@@ -37,34 +58,23 @@
       return '[' + elems.join(', ') + ']';
     };
     
-    var date = function (input) {
-      if (!options.relax.date) return undefined;
-      return string(Time.phputc("Y-m-d\\TH:i:s.u\\Z", input));
-    };
-    
-    var string = function (input) {
-      var result = '', enc;
-      var specials = {
-        '"'  : '\\"',
-        '\b' : '\\b',
-        '\f' : '\\f',
-        '\n' : '\\n',
-        '\r' : '\\r',
-        '\t' : '\\t',
-        '\\' : '\\\\'
+    var object = function (input) {
+      var key = function (k) {
+        if (options.relax.objkey)
+          if ((/^([0-9]+|[A-Za-z$_][A-Za-z0-9$_]*)$/).test(k))
+            return k.toString();
+        return string(k);
       };
-      while (input.length > 0) {
-        if (match = input.match(/[\x00-\x1f\\"]/)) {
-          enc = padnum(2, match[0].charCodeAt(0).toString(16));
-          
-          result += input.slice(0, match.index);
-          result += specials[match[0]] ? specials[match[0]] : ('\\u00' + enc);
-          input  = input.slice(match.index + match[0].length);
-        } else {
-          result += input; input = '';
+      
+      var pairs = [], k, v;
+      for (var prop in input) {
+        if (options.allkey || input.propertyIsEnumerable(prop)) {
+          k = key(prop);
+          v = value(input[prop]);
+          if (Type.is_a(v, String)) pairs.push(k + ': ' + v);
         }
       }
-      return '"' + result + '"';
+      return '{' + pairs.join(', ') + '}';
     };
     
     var value = function (input) {
@@ -76,7 +86,7 @@
         case 'number'   :
         case 'boolean'  : return input.toString();
         case 'null'     : return 'null';
-        case 'undefined': return options.relax.keyword ? 'undefined' : undefined;
+        case 'undefined': if (options.relax.keyword) return 'undefined';
         default : return undefined;
       }
     };
