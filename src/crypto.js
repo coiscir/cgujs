c973904f7dc69c75380ad7aa977ededb68a6526e
Jonathan Lonowski <coiscir@users.noreply.github.com>
2008-12-01 03:54:31 +0000

    Crypto: Minor comment change.

--- src/crypto.js
+++ src/crypto.js
@@ -15,11 +15,11 @@
  *  RIPEMD-128
  *  RIPEMD-160
  *
- **~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
- * MD4 (c) 1990 R. Rivest                                           [RFC 1320]
- * MD5 (c) 1992 R. Rivest                                           [RFC 1321]
- * SHA (c) 2006 The Internet Society                                [RFC 4634]
- * RMD (c) 1996 Hans Dobbertin, Antoon Bosselaers, and Bart Preneel
+ *----
+ *  MD4 (c) 1990 R. Rivest                                          [RFC 1320]
+ *  MD5 (c) 1992 R. Rivest                                          [RFC 1321]
+ *  SHA (c) 2006 The Internet Society                               [RFC 4634]
+ *  RMD (c) 1996 Hans Dobbertin, Antoon Bosselaers, and Bart Preneel
  *
  **~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
  *~~~ Methods
