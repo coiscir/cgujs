25cc7c4f2913b764f24840eb8500b2abef331d56
Jonathan Lonowski <coiscir@users.noreply.github.com>
2009-01-23 09:02:17 +0000

    JSON: Continue r229.

--- test/cycles/iterator.js
+++ test/cycles/iterator.js
@@ -33,18 +33,18 @@ cycles.Iterator = {
         ], [
           'number array', '1, 2, 4, 8, 16, 32', function(){ return CGU.map([0, 1, 2, 3, 4, 5], function (v) { return Math.pow(2, v); }).join(', '); }
         ], [
-          'object', '{foo: "foo", red: "red"}', function(){ return CGU.toJSON(CGU.map(iterObj, function (v, k) { return k; })); }
+          'object', '{foo: "foo", red: "red"}', function(){ return CGU.toJSON(CGU.map(iterObj, function (v, k) { return k; }), false); }
         ]
       ]
     }, {
       title: 'reject',
       cases: [
         [
-          'string array', '["foo"]', function(){ return CGU.toJSON(CGU.reject(iterArr, function (v) { return v === 'bar'; })); }
+          'string array', '["foo"]', function(){ return CGU.toJSON(CGU.reject(iterArr, function (v) { return v === 'bar'; }), false); }
         ], [
           'number array', '1, 3, 5', function(){ return CGU.reject([0, 1, 2, 3, 4, 5], function (v) { return (v % 2) == 0; }).join(', '); }
         ], [
-          'object', '{red: "foo"}', function(){ return CGU.toJSON(CGU.reject(iterObj, function (v) { return v === 'bar'; })); }
+          'object', '{red: "foo"}', function(){ return CGU.toJSON(CGU.reject(iterObj, function (v) { return v === 'bar'; }), false); }
         ]
       ]
     }, {
