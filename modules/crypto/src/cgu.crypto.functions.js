929a9c3602545d41bcacd4037dc31aab05a929ab
Jonathan Lonowski <coiscir@users.noreply.github.com>
2009-03-19 01:54:34 +0000

    Crypto: Swapped `hashable` for `utf8Encode`. Dropped `bytes` arguments and force UTF-8 encoding. Updated test cases.

--- modules/crypto/src/cgu.crypto.functions.js
+++ modules/crypto/src/cgu.crypto.functions.js
@@ -3,52 +3,28 @@
  **~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 **/
   
-  // listable
-  CGU.hashes = function (keyed) {
-    keyed = keyed === !!keyed ? keyed : null; // true, false, or null
-    
-    var calls = [];
-    for (var call in Algos)
-      if (Algos[call] instanceof Algo)
-        if (keyed === null || Algos[call].keyed == keyed)
-          calls.push(call);
-    return calls;
-  };
-  
-  // conversion
-  CGU.hashable = function (str) {
-    for (var out = '', i = 0; i < str.length; i += 1) {
-      out += String.fromCharCode((str.charCodeAt(i) >> 8) & 0xff);
-      out += String.fromCharCode((str.charCodeAt(i) >> 0) & 0xff);
-    }
-    return out;
-  };
-  
-  // core functions
-  CGU.hash = function (call, data, hkey, bytes) {
+  CGU.hash = function (call, data, hkey) {
     if (typeof(call) != 'string') call = null;
     if (typeof(data) != 'string') data = null;
     if (typeof(hkey) != 'string') hkey = null;
-    if (bytes !== true) bytes = false;
     
     if (!(Algos[call] instanceof Algo) || data === null) return;
     if (Algos[call].keyed && hkey === null) return null;
     
-    if (bytes) data = CGU.hashable(data);
+    data = CGU.utf8Encode(data); hkey = CGU.utf8Encode(hkey);
     
     return CGU.Sequence(Algos[call].algo(data, hkey));
   };
   
-  CGU.hmac = function (call, data, hkey, bytes) {
+  CGU.hmac = function (call, data, hkey) {
     if (typeof(call) != 'string') call = null;
     if (typeof(data) != 'string') data = null;
     if (typeof(hkey) != 'string') hkey = null;
-    if (bytes !== true) bytes = false;
     
     if (!(Algos[call] instanceof Algo) || data === null) return;
     if (Algos[call].keyed || hkey === null) return null;
     
-    if (bytes) data = CGU.hashable(data);
+    data = CGU.utf8Encode(data); hkey = CGU.utf8Encode(hkey);
     
     var akey, ihash, ipad = [], opad = [];
     var block = Algos[call].block, klen = hkey.length;
@@ -64,4 +40,33 @@
     
     ihash = CGU.Sequence(Algos[call].algo(ipad + data)).str();
     return CGU.Sequence(Algos[call].algo(opad + ihash));
+  };
+  
+  CGU.hashes = function (keyed) {
+    keyed = keyed === !!keyed ? keyed : null; // true, false, or null
+    
+    var calls = [];
+    for (var call in Algos)
+      if (Algos[call] instanceof Algo)
+        if (keyed === null || Algos[call].keyed == keyed)
+          calls.push(call);
+    return calls;
+  };
+  
+  CGU.utf8Encode = function (string) {
+    if (typeof(string) !== 'string') return string;
+    for (var res = '', c, i = 0; i < string.length; i += 1) {
+      c = string.charCodeAt(i);
+      if (c <= 127) {
+        res += String.fromCharCode(c);
+      } else if (c <= 2047) {
+        res += String.fromCharCode(192 + ((c >> 6) & 0x1f));
+        res += String.fromCharCode(128 + ((c >> 0) & 0x3f));
+      } else {
+        res += String.fromCharCode(224 + ((c >> 12) & 0xf));
+        res += String.fromCharCode(128 + ((c >> 6) & 0x3f));
+        res += String.fromCharCode(128 + ((c >> 0) & 0x3f));
+      }
+    }
+    return res;
   };
