e54a8db9be7b33594ee1670ea239925f810c3e27
jonllmsed <jonllmsed@217474b8-4953-0410-b849-a73ab7b60919>
2009-04-02 17:48:54 +0000

    Crypto: Cleanup from r266.

--- trunk/modules/crypto/src/cgu.crypto.js
+++ trunk/modules/crypto/src/cgu.crypto.js
@@ -33,10 +33,6 @@
 <%= inc opt(2, 0), opt(true, false),
   'cgu.crypto.functions.js',
   'cgu.crypto.sequence.js',
-  'cgu.crypto.oper.{32,64}bit.js',
-  'cgu.crypto.hash.md{4,5}.js',
-  'cgu.crypto.hash.sha{1,2}*.js',
-  'cgu.crypto.hash.ripemd{128,160}.js',
-  'cgu.crypto.hash.sha3.md6.js'
+  'cgu.crypto.oper.{32,64}bit.js'
 %>
 })();
